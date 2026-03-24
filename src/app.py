c4bc20cf5ec7b71c3bc92c635a324cf1d4cd79a4
guilhermehenri456 <guilhermeconelheiro@gmail.com>
2026-03-24 13:35:20 -0300

    Flet

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from enum import IntFlag
 from numbers import Number
 
 import flet
-from flet import ThemeMode, Text, TextField, OutlinedIconButton, OutlinedButton, Column, CrossAxisAlignment
+from flet import ThemeMode, Text, TextField, OutlinedIconButton, OutlinedButton, Column, CrossAxisAlignment, Container, \
+    FontWeight, Colors
 from flet.controls import page
 from flet.controls.border_radius import horizontal
 
@@ -10,86 +11,105 @@ from flet.controls.border_radius import horizontal
 def main(page: flet.Page):
     # Configurações
     page.title = "Primeiro APP"
-    page.theme_mode = ThemeMode.LIGHT   # ou Dark
+    page.theme_mode = ThemeMode.LIGHT  # ou Dark
     page.window.width = 400
     page.window.height = 700
+
     # Funções
     def salvar_nome():
         text.value = f"Bom dia {input_nome.value} {input_sobrenome.value}"
         page.update()
 
-
     # Componentes
-    text = Text("Olá mundo")
+    text = Text("")
     input_nome = TextField(label="Digite seu nome")
     input_sobrenome = TextField(label="Digite seu sobrenome")
     btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
 
-
     # Construção da tela
     page.add(
-        Column(
-            [
-                input_nome,
-                input_sobrenome,
-                btn_salvar,
-                text
-            ],
+        Container(
+            Column(
+                [
+                    Text("Atividade 01", weight=FontWeight.BOLD, size=24),
+                    input_nome,
+                    input_sobrenome,
+                    btn_salvar,
+                    text
+                ],
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+            ),
+            bgcolor=Colors.GREEN_200,
+            padding=15,
+            border_radius=10,
             width=400,
-            horizontal_alignment=CrossAxisAlignment.CENTER
-        )
+        ),
     )
+
     def verificar():
         try:
             n1 = int(input_n1.value)
             if n1 % 2 == 0:
-                text.value = f'{n1} seu número é par'
+                text2.value = f'{n1} seu número é par'
             else:
-                text.value = f'{n1} seu número é impar'
+                text2.value = f'{n1} seu número é impar'
         except ValueError:
-            text.value = f'Erro'
+            text2.value = f'Erro'
 
-    text = Text("")
-    input_n1 = TextField(label="Digite o seu primeiro numero")
+    text2 = Text("")
+    input_n1 = TextField(label="Digite o um numero")
     btn_verificar = OutlinedButton("Verificar", on_click=verificar)
 
     page.add(
-        Column(
-            [
-                input_n1,
-                btn_verificar,
-                text
-            ],
+        Container(
+            Column(
+                [
+                    Text("Atividade 02", weight=FontWeight.BOLD, size=24),
+                    input_n1,
+                    btn_verificar,
+                    text2
+                ],
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+            ),
+            bgcolor=Colors.BLUE_200,
+            padding=15,
+            border_radius=10,
             width=400,
-            horizontal_alignment=CrossAxisAlignment.CENTER
         )
     )
     def nascimento():
         idade = int(input_nascimento.value)
         resultado_idade = 2026 - idade
         if resultado_idade >= 18:
-            text.value = f"Ele tem {resultado_idade} anos. Ele é maior de idade, "
+            text3.value = f"Ele tem {resultado_idade} anos. Ele é maior de idade, "
             page.update()
         else:
-            text.value = f"Ele tem {resultado_idade} anos. Ele é menor de idade, "
+            text3.value = f"Ele tem {resultado_idade} anos. Ele é menor de idade, "
             page.update()
 
     # componentes
     input_nascimento = TextField(label="Digite o ano de nascimento")
-
+    text3 = Text("")
     btn_salvar_tres = OutlinedButton("Salvar", on_click=nascimento)
 
     # Contrução da tela
     page.add(
-        Column(
-            [
-                input_nascimento,
-                btn_salvar_tres,
-            ],
+        Container(
+            Column(
+                [
+                    Text("Atividade 03", weight=FontWeight.BOLD, size=24),
+                    input_nascimento,
+                    btn_salvar_tres,
+                    text3
+                ],
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+            ),
+            bgcolor=Colors.BROWN_200,
+            padding=15,
+            border_radius=10,
             width=400,
-            horizontal_alignment=CrossAxisAlignment.CENTER
         )
     )
 
 
-flet.app(main)
+flet.run(main)
